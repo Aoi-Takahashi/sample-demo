27454fda767429bfb44348f4e5053e1992123c98
Aoi-Takahashi <pythondevelop.forbusines@gmail.com>
2024-07-12 00:38:06 +0900

    feat: server setup(index.ts)

---
+++ server/index.ts
@@ -0,0 +1,43 @@
+import express, { Application, Request, Response } from 'express'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { config } from 'dotenv';
+
+const app: Application = express()
+const PORT = 3001
+config()
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "connected");
+
+  socket.emit("confirmation", "connected!");
+
+  socket.on("event", (data) => {
+    console.log(socket.id, data);
+    socket.emit("event", "pong");
+  });
+});
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
+
+app.get('/', async (_req: Request, res: Response) => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  })
+})
+
+const port = process.env.PORT || PORT;
+
+// instead of running listen on the Express app, do it on the HTTP server
+httpServer.listen(port, () => {
+  console.log(`Express server listening at http://localhost:${port}`);
+});
+
+
